3b2836fa872fb1845dbbc1153645e7a5fa4d6a10
hazarky <kordell1230@gmail.com>
2022-05-08 12:38:17 -0700

    final push

--- apprentice-push.sh
+++ apprentice-push.sh
@@ -10,9 +10,17 @@ function push-overlay () {
     #chown -R www-data:www-data /var/www/html/mod/apprentice
     sudo cp apprentice-site/theme.php /var/www/html/engine/theme.php
     chown -R www-data:www-data /var/www/html/engine/theme.php
+    sudo cp apprentice-site/menus.css /var/www/html/views/default/elements/components/menus.css
+    sudop chown -R www-data:www-data /var/www/html/views/default/elements/components/menus.css
+    sudo cp apprentice-site/admin.css.php /var/www/html/views/default/admin.css.php
+    sudo chown -R www-data:www-data /var/www/html/views/default/admin.css.php
+    sudo cp apprentice-site/tabs.css /var/www/html/views/default/elements/components/tabs.css
+    sudo chown -R www-data:www-data /var/www/html/views/default/elements/components/tabs.css
+    sudo cp apprentice-site/wysiwyg.css /var/www/html/mod/ckeditor/views/default/elgg/wysiwyg.css
+    sudo chown -R www-data:www-data /var/www/html/mod/ckeditor/views/default/elgg/wysiwyg.css
 }
 function main () {
-    printf "Pushing apprentice-overlay"
+    printf "Pushing apprentice-overlay\n"
     push-overlay
 }
 main
